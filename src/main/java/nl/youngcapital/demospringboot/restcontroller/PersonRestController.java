06717dd50f63174f367ab2ce997079490b558a0f
Maarten Dijkstra <maartenrdijkstra@gmail.com>
2021-11-10 15:30:36 +0100

    Cors

--- src/main/java/nl/youngcapital/demospringboot/restcontroller/PersonRestController.java
+++ src/main/java/nl/youngcapital/demospringboot/restcontroller/PersonRestController.java
@@ -1,6 +1,7 @@
 package nl.youngcapital.demospringboot.restcontroller;
 
 import nl.youngcapital.demospringboot.entity.Persoon;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class PersonRestController {
         return "hoi";
     }
 
+    @CrossOrigin(origins = "https://calm-water-079651203.azurestaticapps.net/")
     @GetMapping("endpoint")
     public Persoon endpoint() {
         Persoon persoon = new Persoon("Piet", "Klein");
